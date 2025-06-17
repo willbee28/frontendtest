01c84d6d4e5d40bb3c4b711957f25b07ebd65776
Will <will.t.borwegen@gmail.com>
2025-06-17 13:44:35 -0400

    get it to a beautful working state, scaling both horizontally and vertically with window size

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { nextTick, onMounted, onBeforeUnmount, ref } from "vue";
+import Chessboard, { type LedgerEntry } from "./components/Chessboard.vue";
+
+const ledger = ref<LedgerEntry[]>([]);
+const ledgerContainer = ref<HTMLElement | null>(null);
+const windowHeight = ref(window.innerHeight);
+
+const updateWindowHeight = () => {
+  windowHeight.value = window.innerHeight;
+};
+
+onMounted(() => {
+  window.addEventListener("resize", updateWindowHeight);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", updateWindowHeight);
+});
+
+const handleLedgerUpdate = (newLedgerEntry: LedgerEntry) => {
+  ledger.value.push(newLedgerEntry);
+  nextTick(() => {
+    if (ledgerContainer.value) {
+      ledgerContainer.value.scrollTop = ledgerContainer.value.scrollHeight;
+    }
+  });
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-[#393936] h-screen">
+    <div
+      class="text-white py-8 px-8 text-4xl w-1/2 mx-auto flex justify-center lg:justify-start"
+    >
+      Chess.com
+    </div>
+    <div class="xl:w-1/2 sm:w-3/5 mx-auto">
+      <div
+        class="flex flex-col lg:flex-row gap-4 m-4"
+        :style="{ maxWidth: windowHeight + 'px' }"
+      >
+        <div class="flex flex-grow justify-center items-start">
+          <Chessboard @updateLedger="handleLedgerUpdate" />
+        </div>
+        <div
+          class="flex flex-col bg-[#2b2b2b] text-white rounded-lg p-4 lg:w-44 h-64 lg:h-auto overflow-y-auto custom-scrollbar"
+          ref="ledgerContainer"
+        >
+          <div class="underline">Moves</div>
+          <div v-for="(posit, index) in ledger" :key="posit.letter + posit.num">
+            <span class="">
+              {{ index }}. &nbsp;{{ posit.letter }}{{ posit.num }}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -26,5 +71,9 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.custom-scrollbar {
+  scrollbar-width: thin;
+  scrollbar-color: rgba(255 255 255 / 0.29) transparent;
 }
 </style>
--- src/components/Chessboard.vue
+++ src/components/Chessboard.vue
@@ -1,38 +1,99 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+export type LedgerEntry = { letter: string; num: number };
 
-defineProps<{ msg: string }>()
+const letters = ["h", "g", "f", "e", "d", "c", "b", "a"];
 
-const count = ref(0)
+const squares: LedgerEntry[] = [];
+for (let num = 1; num <= 8; num++) {
+  for (let c = 0; c < 8; c++) {
+    squares.push({ letter: letters[c], num: num });
+  }
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <!-- overflow-hidden is so rounded corners show -->
+  <div class="aspect-square w-full rounded-xl overflow-hidden">
+    <div class="grid grid-cols-8 grid-rows-8 w-full h-full">
+      <span
+        v-for="(square, index) in squares"
+        :key="square.letter + square.num"
+        :class="[
+          'relative w-full h-full',
+          // checkerboard color logic
+          (square.num + index) % 2 === 0 ? 'bg-[#EBECD0]' : 'bg-[#739552]',
+        ]"
+        @click="$emit('updateLedger', square)"
+      >
+        <span v-if="square.letter === 'h'" class="absolute top-0 left-1">
+          {{ square.num }}
+        </span>
+        <span v-if="square.num === 8" class="absolute bottom-0 right-1">
+          {{ square.letter }}
+        </span>
+      </span>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
+
+<!-- <template>
+ 
+  <div class="rounded-xl overflow-hidden">
+    <div v-for="num in numbers" :key="num" class="grid grid-cols-8">
+      <span
+        v-for="(letter, index) in letters"
+        :key="letter + num"
+        :class="[
+          'relative aspect-square',
+          // checkerboard color logic
+          (index + num) % 2 === 0 ? 'bg-[#EBECD0]' : 'bg-[#739552]',
+        ]"
+        @click="$emit('updateLedger', { letter, num })"
+      >
+        <span v-if="letter === 'a'" class="absolute top-0 left-1">{{
+          num
+        }}</span>
+        <span v-if="num === 8" class="absolute bottom-0 right-1">{{
+          letter
+        }}</span>
+      </span>
+    </div>
+  </div>
+</template> -->
+
+<!-- const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const squares = [];
+for (let r = 0; r < 8; r++) {
+  for (let c = 0; c < 8; c++) {
+    squares.push({ letter: letters[c], num: numbers[7 - r], row: r, col: c });
+  }
+}
+</script>
+
+<template>
+  <div class="aspect-square w-full rounded-xl overflow-hidden">
+    <div class="grid grid-cols-8 grid-rows-8 w-full h-full">
+      <span
+        v-for="square in squares"
+        :key="square.letter + square.num"
+        :class="[
+          'relative w-full h-full',
+          (square.row + square.col) % 2 === 0 ? 'bg-[#EBECD0]' : 'bg-[#739552]',
+        ]"
+        @click="$emit('updateLedger', square)"
+      >
+        <span v-if="square.letter === 'a'" class="absolute top-0 left-1">
+          {{ square.num }}
+        </span>
+        <span v-if="square.num === 8" class="absolute bottom-0 right-1">
+          {{ square.letter }}
+        </span>
+      </span>
+    </div>
+  </div>
+</template> -->
 
 <style scoped>
 .read-the-docs {
